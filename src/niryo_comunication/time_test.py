c291de4e12bd4349e080df1cd8b1ee79716a06b5
NicolePfenninger <nicolepf@student.ethz.ch>
2021-12-04 15:49:00 +0100

    test if we can get the info from oBCI processing
    This file connects to the oBCI processing method and gets the information about the channel, the time_difference and the muscle activation

---
+++ src/niryo_comunication/time_test.py
@@ -0,0 +1,16 @@
+from src.oBCI_processing.oBCI_prc_class import Processing
+from src.oBCI_processing.circbuffer import *
+import time
+
+start = True
+P = Processing()
+p = P.processing()
+
+while start == True:
+    time_diff_cha1, time_diff_cha2, time_diff_cha3, activation_cha1, activation_cha2, activation_cha3, loop_time,  muscle = next(p)
+    time.sleep(loop_time)
+    #print("Channel 1\n", "time diff", time_diff_cha1, "activated", activation_cha1)
+    #print("Channel 2\n", "time diff", time_diff_cha2, "activated", activation_cha2)
+    print("Channel 3\n", "time diff", time_diff_cha3, "Msucle activated", muscle, "Robot activated", activation_cha3)
+
+#N = Niryo(time_diff_cha1, time_diff_cha2, time_diff_cha3, activation_cha1, activation_cha2, activation_cha3)
