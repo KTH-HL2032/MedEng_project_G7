7ce4d38afd33137326bf83c4c6e2743b30d56be2
youngsinatra313 <62658540+youngsinatra313@users.noreply.github.com>
2021-12-14 15:56:58 +0100

    Update niryo_src.py
    formating

--- src/niryo_comunication/niryo_src.py
+++ src/niryo_comunication/niryo_src.py
@@ -5,9 +5,6 @@ import math
 from pynput import keyboard
 
 
-
-
-
 #  KEYS FOR MOVING
 #  'w': End effector moves in the direction of positive x-axis
 #  's': End effector moves in the direction of negative x-axis
@@ -65,21 +62,24 @@ class Niryo:
             print("No direction key")
             return False
 
-        shift = pre1*self.time_taken * 0.1  # sets shift amount
+        shift = pre1 * self.time_taken * 0.1  # sets shift amount
         changing_dir = shift + pos_list[changing_index]  # computes end position changing axis
         fix_dir = pos_list[fix_index]  # end position on fix axis
         if math.sqrt(changing_dir ** 2 + fix_dir ** 2) <= in_rad:  # checks if end_position is beyond set inner radius
             changing_dir = math.sqrt(in_rad ** 2 - fix_dir ** 2)  # computes new min end position on changing axis
-            shift = pre_i*changing_dir - pos_list[changing_index]  # computes new max shift that is still allowed to keep
+            shift = pre_i * changing_dir - pos_list[
+                changing_index]  # computes new max shift that is still allowed to keep
             # the arm above inner radius
 
         if math.sqrt(changing_dir ** 2 + fix_dir ** 2) >= o_rad:  # checks if end_position is beyond set outer radius
             changing_dir = math.sqrt(o_rad ** 2 - fix_dir ** 2)  # computes new max end position on changing axis
-            shift = pre_o*changing_dir - pos_list[changing_index]  # computes new max shift that is still allowed to keep
+            shift = pre_o * changing_dir - pos_list[
+                changing_index]  # computes new max shift that is still allowed to keep
             # the arm in below outer radius
         return shift
 
-    def on_key_release(self,key):  # Everything happens when the button is released so it knows how long the button was pressed
+    def on_key_release(self,
+                       key):  # Everything happens when the button is released so it knows how long the button was pressed
 
         status_pos, data_pos = self.niryo_one_client.get_pose()
         pos_list = PoseObject.to_list(data_pos)
@@ -132,8 +132,6 @@ class Niryo:
                 print("Error: " + data)
 
 
-
 while True:
-
     with keyboard.Listener(on_release=Niryo.on_key_release) as release_listener:
         release_listener.join()
